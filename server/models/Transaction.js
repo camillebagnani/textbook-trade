e99fadcf5a12c5ddbfb2a71cb9e9b4236180071e
williamrankin <william.rankin01@gmail.com>
2024-04-03 06:01:45 -0600

    corrected model

--- server/models/Transaction.js
+++ server/models/Transaction.js
@@ -20,6 +20,6 @@ const transactionSchema = new Schema({
     }
   });
   
-  const Transaction = mongoose.model('Transaction', transactionSchema);
+  const Transaction = model('Transaction', transactionSchema);
   
   module.exports = Transaction;
