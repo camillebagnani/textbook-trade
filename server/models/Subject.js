4c34db40ec3b34e4899c438500b054c0ee00b677
williamrankin <william.rankin01@gmail.com>
2024-04-03 06:01:28 -0600

    do we need to add book?

--- server/models/Subject.js
+++ server/models/Subject.js
@@ -6,7 +6,13 @@ const subjectSchema = new Schema({
     type: String,
     required: true,
     trim: true
-  }
+  },
+  // books: [
+  //   {
+  //     type: Schema.Types.ObjectId,
+  //     ref: 'Books'
+  //   }
+  // ]
 });
 
 const Subject = model('Subject', subjectSchema);
