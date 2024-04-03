3c5e8680546a4322e027268032768ea23c729160
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-04-02 20:52:54 -0600

    added queries on front end

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,70 +1,111 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_PRODUCTS = gql`
-  query getProducts($category: ID) {
-    products(category: $category) {
-      _id
-      name
-      description
-      price
-      quantity
+export const QUERY_ME = gql`
+query me {
+  me {
+    _id
+    username
+    books {
+      authors
       image
-      category {
-        _id
-      }
-    }
-  }
-`;
-
-export const QUERY_CHECKOUT = gql`
-  query getCheckout($products: [ProductInput]) {
-    checkout(products: $products) {
-      session
-    }
-  }
-`;
-
-export const QUERY_ALL_PRODUCTS = gql`
-  {
-    products {
-      _id
-      name
-      description
+      isbn
       price
-      quantity
-      category {
-        name
+      sold
+      subject
+      title
+    }
+    transactions {
+      book {
+        authors
+        image
+        isbn
+        price
+        sold
+        subject
+        title
       }
-    }
-  }
-`;
-
-export const QUERY_CATEGORIES = gql`
-  {
-    categories {
-      _id
-      name
-    }
-  }
-`;
-
-export const QUERY_USER = gql`
-  {
-    user {
-      firstName
-      lastName
-      orders {
+      purchaseDate
+      sellerId {
         _id
-        purchaseDate
-        products {
-          _id
-          name
-          description
-          price
-          quantity
-          image
-        }
+        username
+      }
+      buyerId {
+        _id
+        username
       }
     }
   }
+}
 `;
+
+export const QUERY_ALL_BOOKS = gql`
+query books {
+  books {
+    authors
+    image
+    isbn
+    price
+    sold
+    subject
+    title
+    user {
+      username
+    }
+  }
+}`
+
+export const QUERY_ALL_SUBJECTS = gql `
+query subjects {
+  subjects {
+    name
+    books {
+      authors
+      image
+      isbn
+      price
+      sold
+      subject
+      title
+      user {
+        username
+      }
+    }
+  }
+}`
+;
+
+export const SINGLE_BOOK = gql`
+query book($id: ID!) {
+  book(_id: $id) {
+    authors
+    image
+    isbn
+    price
+    sold
+    subject
+    title
+    user {
+      username
+    }
+  }
+}
+`
+export const SINGLE_SUBJECT = gql `
+query subject($name: String!) {
+  subject(name: $name) {
+    name
+    books {
+      authors
+      image
+      isbn
+      price
+      sold
+      subject
+      title
+      user {
+        username
+      }
+    }
+  }
+}
+`
