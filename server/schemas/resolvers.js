c0d2e85c932fdaaf5b83bf8c8c901294a4302e61
williamrankin <william.rankin01@gmail.com>
2024-04-04 09:29:16 -0600

    updated me, added transaction

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,9 +3,32 @@ const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
   Query: {
-    me: async (parent, args, context) => {
-      if (context.user) {
-        return await User.findOne({ _id: context.user._id });
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return await User.findOne({ _id: context.user._id })
+          .populate('books')
+          .populate({
+            path: 'transactions',
+            populate: {
+              path: 'book',
+              model: 'book'
+            }
+          })
+          .populate({
+            path: 'transactions',
+            populate: {
+              path: 'sellerId',
+              model: 'user'
+            }
+          })
+          .populate({
+            path: 'transactions',
+            populate: {
+              path: 'buyerId',
+              model: 'user'
+            }
+          })
+          ;
         //populate transactions and books?
       }
       throw AuthenticationError;
@@ -19,14 +42,20 @@ const resolvers = {
     subjects: async () => {
       return await Subject.find();
     },
-    subject: async (parent, args) => {
-      //const params = {};
+    subject: async (parent, {subject}) => {
 
-      // if (args) {
-      //   params.subject = args;
-      // }
-      return await Subject.findById(args._id).populate("book");
+      return await Book.find({subject: subject}).populate('subject');
     },
+    transactions: async () => {
+      return await Transaction.find()
+      .populate({
+        path: 'book',
+        populate: {
+          path: 'book',
+          model: 'book'
+        }
+      })
+    }
   },
 
   Mutation: {
@@ -112,6 +141,7 @@ const resolvers = {
       throw AuthenticationError;
     },
     addTransaction: async (parent, args, context) => {
+      console.log(args)
       if (context.user) {
         const newTransaction = await Transaction.create(
           {
@@ -128,7 +158,20 @@ const resolvers = {
             new: true,
           }
         );
-        return { newTransaction, book };
+        const buyer = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          {
+            $addToSet: { transactions: newTransaction._id },
+          }
+        )
+        const seller = await User.findOneAndUpdate(
+          { _id: args.sellerId },
+          {
+            $addToSet: { transactions: newTransaction._id },
+          }
+        )
+
+        return { newTransaction, book};
       }
       throw AuthenticationError;
     },
