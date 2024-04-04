b0b2b28f16b8791fe23782094c4ef518c75a6475
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-04-03 19:22:37 -0600

    Updated navbar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -5,13 +5,14 @@ function Nav() {
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/user/:id">Account</Link>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/user/:id">
+              Profile
+            </a>
           </li>
-          <li className="mx-1">
-            {/* this is not using the Link component to logout or user and then refresh the application to the start */}
-            <a href="/" onClick={() => Auth.logout()}>
+          <li class="nav-item">
+            <a class="nav-link" onClick={() => Auth.logout()}>
               Logout
             </a>
           </li>
@@ -19,12 +20,16 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">Signup</Link>
+        <ul class="nav justify-content-end">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/signup">
+              Signup
+            </a>
           </li>
-          <li className="mx-1">
-            <Link to="/login">Login</Link>
+          <li class="nav-item">
+            <a class="nav-link" href="/login">
+              Login
+            </a>
           </li>
         </ul>
       );
@@ -32,27 +37,18 @@ function Nav() {
   }
 
   return (
-    <header className="flex-row px-1">
+    <header className="flex-row px-1 shadow-sm mb-5">
       <nav class="navbar bg-body-tertiary">
         <div class="container-fluid">
           <a class="navbar-brand" href="/">
-            <span role="img" aria-label="textbook">
-              📚
-            </span>
-            Textbook Trade
+            <h1>
+              <span role="img" aria-label="textbook">
+                📚
+              </span>
+              Textbook Trade
+            </h1>
           </a>
-          <ul class="nav justify-content-end">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/signup">
-                Signup
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/login">
-                Login
-              </a>
-            </li>
-          </ul>
+          { showNavigation() }
         </div>
       </nav>
     </header>
