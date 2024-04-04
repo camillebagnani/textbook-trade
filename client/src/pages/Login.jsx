e185bb83fbd10d6962e95b369074090b3e21c620
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-04-03 19:09:36 -0600

    Added container to App.jsx. Created visual aspect of Signup page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,46 @@
 function Login() {
   return (
-    <h1>Login</h1>
-  )
+    <div class="card">
+      <div class="card-body">
+        <h4 class="card-title mb-3">Signup</h4>
+        <form>
+        <div class="mb-3">
+            <label for="usernameInput" class="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="usernameInput"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="emailInput" class="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="emailInput"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="passwordInput" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="passwordInput"
+            />
+          </div>
+          <button type="submit" class="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
