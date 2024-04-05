e0d6ebbaf51912c71fb32eb4081c3e8cad4bbce1
camillebagnani <140850865+camillebagnani@users.noreply.github.com>
2024-04-05 16:25:50 -0600

    updated server js with static file path

--- server/server.js
+++ server/server.js
@@ -21,11 +21,17 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
 
-  app.use('/graphql', expressMiddleware(server, {context: authMiddleware}));
+  app.use('/graphql', expressMiddleware(server, {
+      context: authMiddleware
+    }));
 
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
   }
 
   db.once('open', () => {
