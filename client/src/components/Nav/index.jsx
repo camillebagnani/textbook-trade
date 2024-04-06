bc040d2e297c61f8230a5ff4c5aedcda5aa61c5f
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-04-06 13:56:47 -0600

    Added dark theme toggle to nav bar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
 import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
 
 function Nav() {
+  const [darkTheme, setTheme] = useState(() => {
+    const storedTheme = localStorage.getItem("darkTheme");
+    return storedTheme ? JSON.parse(storedTheme) : false;
+  });
+
+  useEffect(() => {
+    if (darkTheme) {
+      document.documentElement.setAttribute("data-bs-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-bs-theme", "light");
+    }
+
+    localStorage.setItem("darkTheme", JSON.stringify(darkTheme));
+  }, [darkTheme]);
+
+  function handleTheme() {
+    if (darkTheme) {
+      setTheme(false);
+    } else {
+      setTheme(true);
+    }
+  }
+
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
@@ -48,7 +71,20 @@ function Nav() {
               Textbook Trade
             </h1>
           </a>
-          { showNavigation() }
+          <div className="form-check form-switch">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="darkThemeSwitch"
+              onClick={handleTheme}
+              defaultChecked={darkTheme}
+            />
+            <label className="form-check-label" forhtml="darkThemeSwitch">
+              Dark theme
+            </label>
+          </div>
+          {showNavigation()}
         </div>
       </nav>
     </header>
