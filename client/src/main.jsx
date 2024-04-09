7433948e9e2a90e3f3107dfb568668f61d31d523
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2024-04-08 19:29:29 -0600

    Implemented function into main.jsx to protect routes when not logged in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import { React, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import Home from "./pages/Home.jsx";
 import NoMatch from "./pages/NoMatch.jsx";
 import Signup from "./pages/Signup";
-import User from "./pages/User"
+import User from "./pages/User";
 import SubjectSearch from "./pages/subjectSearch.jsx";
+import Auth from "./utils/auth.js";
+
+const ProtectedRoute = ({ element, path }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!Auth.loggedIn()) {
+      navigate("/");
+    }
+  }, [navigate]);
+
+  return <>{element}</>;
+};
 
 const router = createBrowserRouter([
   {
@@ -24,14 +41,13 @@ const router = createBrowserRouter([
         element: <Signup />,
       },
       {
-        path: '/user',
-        element: <User />,
+        path: "/user",
+        element: <ProtectedRoute path="/user" element={<User />} />,
       },
       {
-        path: '/subject',
-        element: <SubjectSearch />
+        path: "/subject",
+        element: <ProtectedRoute path="/subject" element={<SubjectSearch />} />,
       },
-      
     ],
   },
 ]);
